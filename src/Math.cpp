ca171c472ebcbd3d93ea38b5ec49dbcc1af853ea
KseniyaAndreevna <bindyurova.kseniya@gmail.com>
2014-05-28 21:22:01 +0700

    Issue #9.   Заменила в файле Math.cpp численную константу на именованную.

--- src/Math.cpp
+++ src/Math.cpp
@@ -1,9 +1,10 @@
 #include "Math.h"
 
+#define _USE_MATH_DEFINES
 #include <cmath>
 
 using namespace TexturalAnalysis;
 
 double Math::ln(double x) {
-    return log10(x) / log10(2.7182818284959045);
+    return log10(x) / log10(M_E);
 }
