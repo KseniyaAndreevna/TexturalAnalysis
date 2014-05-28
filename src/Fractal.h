895dbb5dd08431d7ee86d624aaf5d0cd7136b1cc
KseniyaAndreevna <bindyurova.kseniya@gmail.com>
2014-05-29 02:52:18 +0700

    Issue #12.   Оптимизировала передачу параметров, связанных с std::vector

--- src/Fractal.h
+++ src/Fractal.h
@@ -25,15 +25,15 @@ namespace TexturalAnalysis {
         static std::vector<std::vector<Point>> calculatePointsSerias(const Image& image, const int x1, const int y1, const int x2, const int y2);
 
     private:
-        static double distanse(const std::vector<std::vector<double>> phasePoints, const int i, const int j, const int dimension);
+        static double distanse(const std::vector<std::vector<double>>& phasePoints, const int i, const int j, const int dimension);
 
         static int N(const int initialPointsCount, const int dimension);
 
         static int L(const int dimension);
 
-        static double C(const std::vector<std::vector<double>> phasePoints, const int r, const int initialPointsCount, const int dimension);
+        static double C(const std::vector<std::vector<double>>& phasePoints, const int r, const int initialPointsCount, const int dimension);
 
-        static int heaviside(const std::vector<std::vector<double>> phasePoints, const int r, const int i, const int j, const int dimension);
+        static int heaviside(const std::vector<std::vector<double>>& phasePoints, const int r, const int i, const int j, const int dimension);
     };
 }
 
